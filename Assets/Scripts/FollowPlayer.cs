b971ecfe5414adc8712ead6177e3c638b9fa4f1c
DewulfTjorven <tjorven.dewulf@student.howest.be>
2020-11-19 00:01:50 +0100

    added movement

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,16 +6,24 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
+    public GameObject child;
+    private Vector3 offset = new Vector3(0, 2, -2);
+    private float speed = 7.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        follow();
+
+    }
+
+    private void follow(){
+        transform.position = Vector3.Lerp(transform.position, child.transform.position, Time.deltaTime * speed);
+        transform.LookAt(player.transform.position);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 40.0f;
+    private float turnSpeed = 80.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+
     }
 }
