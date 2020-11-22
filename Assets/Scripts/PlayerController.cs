015dfe0f85820ac4c07bf9db77ad92246f9c3005
DewulfTjorven <tjorven.dewulf@student.howest.be>
2020-11-22 20:48:12 +0100

    collision makes me drive slower closes #31

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 45.0f;
-    private float turnSpeed = 85.0f;
+    public float speed = 75.0f;
+    private float turnSpeed = 55.0f;
     private float horizontalInput;
     private float forwardInput;
-
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +17,23 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        forwardInput = Mathf.Lerp (forwardInput,Input.GetAxis ("Vertical"),Time.deltaTime*0.3f);
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
+        // Auto vertragen achter collision, misschien nog proberen met extra tijd?
+        void OnTriggerEnter(Collider other){
+            speed /= 9.0f;
+        }
 
+        void OnTriggerExit(Collider other){
+            speed *= 9.0f;
+        }
     }
+
+    
 }
